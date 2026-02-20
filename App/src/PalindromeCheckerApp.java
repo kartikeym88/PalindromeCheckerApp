9880e8be41873b71eddfcfa1a6b64d12331fff29
kartikeym88 <madaankartikey@gmail.com>
2026-02-20 10:15:51 +0530

    Merge remote changes into WelcomePage 1

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+// version 1.0
+//author Kartikey
+//useCase 1: Welcome page
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("     Welcome to Palindrome Checker App");
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Version          : 1.0");
+        System.out.println("This application checks whether a given");
+        System.out.println("string is a palindrome.");
+        System.out.println("Application Started Successfully!");
+    }
+}
